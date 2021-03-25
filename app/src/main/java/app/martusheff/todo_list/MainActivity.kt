8904caca015faf0269fc48536f70f7572d375e7a
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-03-24 23:19:36 -0700

    project start
    setting up dev environment

---
+++ app/src/main/java/app/martusheff/todo_list/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.martusheff.todo_list
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
